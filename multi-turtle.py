c8e30455551e1bb3b96b6db1de64b2f3626aa6b1
UnityMaster0 <98353590+UnityMaster0@users.noreply.github.com>
2022-09-26 09:10:58 -0400

    Improved random and added circle functions
    Random function now has goto click functionality
    New circle function draws a circle of an inputed size in the center of the turtle window

--- multi-turtle.py
+++ multi-turtle.py
@@ -2,17 +2,72 @@ import random as rndm
 import turtle as trtl
 import time
 
+#Defines Turtle variable
+t = trtl.Turtle()
 
-def rmdrawing():
+#Controller function for drawSpiral()
+def fullspiral():
+  
+  screen = trtl.Screen()
+  screen.onclick(drawSpiral)
 
-    t = trtl.Turtle()
+  wn = trtl.Screen()
+  wn = trtl.mainloop()
 
-    timerCount = trtl.numinput('Timer Set', 'How long do you want the program to run?: ')
-    print('Running for', timerCount, 'seconds')
+def fullrandom():
+  
+  screen = trtl.Screen()
+  screen.onclick(rmdrawing)
+
+  wn = trtl.Screen()
+  wn = trtl.mainloop()
+
+#Draw spirals based on screen clicks
+def drawSpiral(x, y):
+  t.penup()
+  t.goto(x, y)
+  t.pendown()
+
+#Ask for angle input after every click
+  angle = trtl.numinput('Angle', 'What do you want the angle of the spiral to be?: ')
+  
+  p = 0
+
+#Draw spiral for 100 interations
+  while (p == 0):
+    count = 1
+    while count < 100:
+      for multiColor in ['red','orange','yellow','green','blue','purple']:
+        count += 1
+        t.color(multiColor)
+        t.forward(count)
+        t.speed(count)
+        t.right(angle)
+    break
+
+#Create variables x and y
+def y_x_var(x, y):
+  global var_x, var_y
+  var_x = x
+  var_y = y
+
+#Controller function for rmdrawing()
+def rndminput():
+  global timerCount
+  global start
+  timerCount = trtl.numinput('Timer Set', 'How long do you want the program to run?: ')
+  start = time.time()
+  print('Running for', timerCount, 'seconds')
+  
+  
+#Create psudo-random drawing for an inputed amount of seconds
+def rmdrawing(x, y):
+
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
 
     i = 0
-
-    start = time.time()
 
     while (i == 0):
       for multiColor in ['red','orange','yellow','green','blue','purple']:
@@ -28,49 +83,27 @@ def rmdrawing():
         if end - start >= timerCount:
             i = 1
             print('Drawing complete')
+            wn = trtl.Screen()
+            wn = trtl.mainloop()
 
+#Draw a circle of inputed size
+def drawCircle():
+  size = trtl.numinput('Set Circle Size', 'What do you want the size of the circle to be?: ')
+  
+  t.circle(size)
 
-def fullspiral():
+  wn = trtl.Screen()
+  wn = trtl.mainloop()
 
-    angle = trtl.numinput('Angle', 'What do you want the angle of the spiral to be?: ')
+#Input to select random, spiral, or circle
+setting = trtl.textinput('Mode Select','Do you want to draw random, spiral, or circle (r or s or c)?: ')
 
-    t = trtl.Turtle()
+if setting == 'r' or setting == 'random' or setting == 'Random':
+  rndminput()
+  fullrandom()
 
-    def drawCircle(x, y):
-        t.penup()
-        t.goto(x, y)
-        t.pendown()
+if setting == "s" or setting == 'spiral' or setting == 'Spiral':
+  fullspiral()
 
-        p = 0
-
-        while (p == 0):
-            count = 1
-            while count < 100:
-                for multiColor in ['red','orange','yellow','green','blue','purple']:
-                  count += 1
-                  t.color(multiColor)
-                  t.forward(count)
-                  t.speed(count)
-                  t.right(angle)
-            break
-
-    def goto_var(x, y):
-      global var_x, var_y
-      var_x = x
-      var_y = y
-
-    t.penup()
-    screen = trtl.Screen()
-    screen.onclick(drawCircle)
-
-    wn = trtl.Screen()
-    wn = trtl.mainloop()
-
-
-setting = trtl.textinput('Mode Select','Do you want to draw random or spiral (r or s)?: ')
-
-if setting == 'r':
-    rmdrawing()
-
-if setting == "s":
-    fullspiral()
+if setting == "c" or setting == 'circle' or setting == 'Circle':
+  drawCircle()
